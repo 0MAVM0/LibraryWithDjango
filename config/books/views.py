d94b7f1bb8c8671ae0b4ce04a15e14f1dc74a1e6
0MAVM0 <ediconpts9@gmail.com>
2025-03-18 17:38:56 +0500

    Urls, Views & Template (home_page)

--- config/books/views.py
+++ config/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def home_page(request):
+    books = Book.objects.all()
+    context = { "books" : books }
+
+    return render(request, "home.html", context)
