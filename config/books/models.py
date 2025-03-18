628b550b64e5d19f58273d1192315e1de66745c3
0MAVM0 <ediconpts9@gmail.com>
2025-03-18 17:19:02 +0500

    Registration & creation of Model - Book.

--- config/books/models.py
+++ config/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    author = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
